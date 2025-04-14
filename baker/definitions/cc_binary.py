7daab9e72f1701a06154d49125205e5c1ff6dd4a
xndcn <xndchn@gmail.com>
2025-04-14 20:30:04 +0800

    Add support of building aidl (#9)
    Add support of filegroup
    Add support of .aidl, .yy, .ll sources transform

--- baker/definitions/cc_binary.py
+++ baker/definitions/cc_binary.py
@@ -14,6 +14,9 @@ class CCBinary(Module):
         name = self._get_property("name")
 
         lines.append(f'add_executable({name})')
+        # hack for no srcs
+        lines.append(f'target_sources({name} PRIVATE ".")')
+        lines.append(f'set_target_properties({name} PROPERTIES LINKER_LANGUAGE CXX)')
         lines += self._convert_module_properties_to_cmake(name)
 
         return lines
--- cmake/sources.cmake
+++ cmake/sources.cmake
@@ -9,6 +9,38 @@ function(baker_transform_source_file target SCOPE SOURCE_FILE)
         string(SUBSTRING ${file_name} 1 -1 dependency)
         set(SOURCE_FILE "")
         target_link_libraries(${target} ${SCOPE} ${dependency})
+    elseif(file_ext STREQUAL ".aidl")
+        get_filename_component(dir_path ${SOURCE_FILE} DIRECTORY)
+        file(RELATIVE_PATH dir_path "${CMAKE_CURRENT_SOURCE_DIR}" "${dir_path}")
+        set(output_path ${CMAKE_CURRENT_BINARY_DIR}/gen/$<TARGET_PROPERTY:${target},_path>)
+        set(output_file ${CMAKE_CURRENT_BINARY_DIR}/gen/${dir_path}/${file_name}.cpp)
+        set(import_path ${CMAKE_CURRENT_SOURCE_DIR}/$<TARGET_PROPERTY:${target},_path>)
+        # Run aidl on the .aidl file
+        add_custom_command(
+            OUTPUT ${output_file}
+            COMMAND aidl --lang=cpp
+                -h ${output_path}
+                -o ${output_path}
+                $<LIST:TRANSFORM,${import_path},PREPEND,-I>
+                ${SOURCE_FILE}
+            COMMAND_EXPAND_LISTS
+        )
+        set(SOURCE_FILE "${output_file}")
+        target_include_directories(${target} ${SCOPE} ${output_path})
+    elseif(file_ext STREQUAL ".yy")
+        find_package(BISON)
+        get_filename_component(file_path ${SOURCE_FILE} ABSOLUTE)
+        # Run bison_target on the .yy file
+        bison_target(${FILE_NAME}_parser ${file_path} "${CMAKE_CURRENT_BINARY_DIR}/${file_name}.cpp" DEFINES_FILE ${CMAKE_CURRENT_BINARY_DIR}/${file_name}.h)
+        set(SOURCE_FILE ${BISON_${FILE_NAME}_parser_OUTPUTS})
+        target_include_directories(${target} ${SCOPE} ${CMAKE_CURRENT_BINARY_DIR})
+    elseif(file_ext STREQUAL ".ll")
+        find_package(FLEX)
+        get_filename_component(FILE_PATH ${SOURCE_FILE} ABSOLUTE)
+        # Run flex_target on the .ll file
+        flex_target(${file_name}_lexer ${FILE_PATH} "${CMAKE_CURRENT_BINARY_DIR}/${file_name}.cpp")
+        set(SOURCE_FILE ${FLEX_${file_name}_lexer_OUTPUTS})
+        target_include_directories(${target} ${SCOPE} ${CMAKE_CURRENT_BINARY_DIR})
     endif()
     set(SOURCE_FILE "${SOURCE_FILE}" PARENT_SCOPE)
 endfunction(baker_transform_source_file)
